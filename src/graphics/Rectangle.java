7f8f71efd00a6aec613d97a9c8334e9646df3aa9
MizoeFumiya <fumiya.mizoe@dartslive.com>
2020-07-19 19:42:53 +0900

    2020/07/19 追加

--- src/graphics/Rectangle.java
+++ src/graphics/Rectangle.java
@@ -1,6 +1,9 @@
 package graphics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rectangle extends Shape
 {
-	static final List<CustomVertex> 
+	static final List<CustomVertex> VERTICES = new ArrayList<CustomVertex>();
 }
